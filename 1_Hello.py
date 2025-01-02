750292dd0829b3fb2c60331103b6d8ea23d7e98e
dnyaneshwar <dnyaneshwarmore320@gmail.com>
2025-01-02 18:05:43 +0530

    updated file

--- 1_Hello.py
+++ 1_Hello.py
@@ -1,2 +1,4 @@
-print("hello")
-print("20+30")
+print("hello Dnyaneshwar")    # This will simply print the characheter(string) inside the Double Inverte Comma " " 
+print("20+30")     # This will also Conidered As a string
+print(20+30)      # Addition will performed and output willbe given :- 50
+ 
