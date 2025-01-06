d2cf84134830ffcd418b15397509cf48d0bfdcc0
Dnyaneshwar more <dnyaneshwarmore2212@gmail.com>
2025-01-06 11:33:29 +0530

    upated File

--- 28_List_function2.py
+++ 28_List_function2.py
@@ -1,24 +1,34 @@
 # let's  Undrestand the other function of the list like count,min,max,sort,reverse,index, etc
 
 # lets consider the a simple list,
-li=[10,40,27,60,41,52,"Dnyaneswhar",'Aarushi','dilip',30,19.10,20,50]
+li=[10,40,27,60,41,52,30,19.10,20,50,10]
+strlist= ['Dnyanesh','More','Chhaya','Amruta','Vishal','Manoj']
 
-#  1Count function()
-c=li.count()                # this will return the count of totale value into list
-print(c)
+#  1.Count function()
+c=li.count(10)                # this will return the count of total value into list(Works same with list and string
+st=strlist.count('More')
+print(c,"\t",st)
 
 #  2.Max function()
-print(li.max())                # this will return maximun value into the List
-m=li.max()
+m=max(li)                       # this will return maximun value into the List
 print("This is Max Value: ",m)
+strmax=max(strlist)
+print("The maxstring in List: ",strmax)       # Return Maximiun String into the List
 
 #  3.Min function
-mi=li.min()
+mi=min(li)                                      #this will find min function
 print("The Smallest value of list : ",mi)
+stmin=min(strlist)
+print("The Minimum string into the list : ",stmin)
+
 
 #  4.Sort Function()
-print(li.sort())            # This Will sort the list
+li.sort()
+strlist.sort()
+print('The Sorted list  is : ',li ," \t", strlist)
+# This Will sort the list
 
 #  5.Reverse function
 li.reverse()
-print(li)
+strlist.reverse()
+print("The  Reverse List is ",li,"\n Reverse String List: ",strlist)           # Reversing the Whole List
